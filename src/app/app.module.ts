b858b8d80e528cb754a08122788f5dc21bd3f2bb
MalasiewiczMateusz <mateusz-michal.malasiewicz@capgemini.com>
2022-03-16 17:20:26 +0100

    ejemplos pipes personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LetrasPipe } from './pipe/letras.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetrasPipe
   ],
   imports: [
     BrowserModule,
